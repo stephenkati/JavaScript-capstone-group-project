9c782f9d1b6494d13ae49b5d6e09bbb3f887486e
stephenkati <stephenkatuli88@gmail.com>
2023-03-14 12:21:37 +0300

    setup appID

---
+++ src/modules/setup-involvementAPi.js
@@ -0,0 +1,20 @@
+const involvementAPIURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
+
+const createApp = async () => {
+    const response = await fetch(involvementAPIURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            name: 'HomeFinder',
+        }),
+    });
+
+    const data = await response.text()
+    console.log(data)
+}
+
+// createApp();
+
+appId = 'JIjFwoEkLZESIoGzrK2r';
