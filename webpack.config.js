31a73489c48b02cfd01cdb08fc4ded37a41a1d66
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-03-14 06:52:53 +0000

    update babel setup

--- webpack.config.js
+++ webpack.config.js
@@ -25,14 +25,4 @@ module.exports = {
       },
     ],
   },
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current',
-        },
-      },
-    ],
-  ],
 };
