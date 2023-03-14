cc8a1fa2c64ad71cbfbc8253caa30804492a16ce
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-03-14 12:25:45 +0000

    export and implementation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import './style.css';
 import {getPokemon, pokemonList} from './modules/getPokemon.js';
+import showPopup, {closePopup}from './modules/comment-popup';
 
 pokemonList.forEach((pokemon, index) => {
     getPokemon(pokemon, index)
 })
+
+
+showPopup()
+closePopup()
--- src/modules/comment-popup.js
+++ src/modules/comment-popup.js
@@ -42,3 +42,5 @@ const closePopup = () => {
     }
   })
 } 
+
+export {closePopup}
