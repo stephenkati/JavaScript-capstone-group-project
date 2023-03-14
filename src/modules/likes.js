e67f08b85e22a753113a58f83f13d260ec42bbec
stephenkati <stephenkatuli88@gmail.com>
2023-03-14 14:35:05 +0300

    Display likes on each card

---
+++ src/modules/likes.js
@@ -0,0 +1,38 @@
+import {involvementAPIURL, appId} from './setup-involvementAPi.js'
+import { pokemonList } from './getPokemon.js';
+
+const addLike = async (index) => {
+    const response = await fetch(`${involvementAPIURL}${appId}/likes`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            item_id: `${index}`,
+        }),
+    });
+
+    const data = await response.text()
+}
+
+
+const getLike = async (index) => {
+    const response = await fetch(`${involvementAPIURL}${appId}/likes`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    const data = await response.json();
+
+    data.forEach(element => {
+        if(parseInt(element.item_id) === index){
+            const likes = document.querySelector(`#likes-${index}`)
+            likes.innerHTML = `${element.likes} likes`
+        }
+    });
+
+}
+
+export { addLike, getLike }
