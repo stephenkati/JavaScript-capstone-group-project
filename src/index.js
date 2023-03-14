4d61517ad1e6af2efc0110c192f21ba12733edb7
stephenkati <stephenkatuli88@gmail.com>
2023-03-14 14:36:26 +0300

    show likes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './style.css';
 import {getPokemon, pokemonList} from './modules/getPokemon.js';
+import { addLike, getLike } from './modules/likes.js'
 
 pokemonList.forEach((pokemon, index) => {
     getPokemon(pokemon, index)
+    // addLike(index)
+    getLike(index)
 })
